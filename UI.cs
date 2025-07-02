05755b8c34a80f0685e3066a9ea9fe687b7a5c8b
Bozhidar Enev <enevbozhidar@gmail.com>
2025-07-02 23:37:51 +0300

    Added method ShowAllEvents

--- UI.cs
+++ UI.cs
@@ -10,8 +10,23 @@ namespace SportEvents
     {
         public static void ShowAllEvents(List<Events> events) //показва всички събития
         {
+            int i = 0;
+            while (i < events.Count)
+            {
+                var ev = events[i];
+                Console.WriteLine($"ID: {ev.eventID}");
+                Console.WriteLine($"Име: {ev.Name}");
+                Console.WriteLine($"Местоположение: {ev.Location}");
+                Console.WriteLine($"Дата: {ev.Date:dd.MM.yyyy}");
+                Console.WriteLine($"Билети: {ev.TicketsAvailable}");
+                Console.WriteLine($"Цена: {ev.Price} лв.");
+
+                i++;
+            }
 
         }
+
+
 
     }
 }
