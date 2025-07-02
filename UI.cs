3b1ea570bbeaae339a7ab4d62d2dcf43dae52292
крис <krisiivanov08@gmail.com>
2025-07-02 20:20:34 +0300

    Added Class for UI

---
+++ UI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportEvents
+{
+    internal class UI
+    {
+        public static void ShowAllEvents(List<Events> events) //показва всички събития
+        {
+            Console.Clear();
+            foreach (var e in events)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+    }
+}
