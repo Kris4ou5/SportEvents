f199ccd9766a61f5f647cd6cb20ed0bdcc9148e1
Bozhidar Enev <enevbozhidar@gmail.com>
2025-07-02 20:31:04 +0300

    added AddEvent Functions

--- Functions.cs
+++ Functions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,62 @@ namespace SportEvents
     internal class Functions
     {
 
+        private static void AddEvent()
+        {
+
+       
+            Console.WriteLine("--- Добавяне на ново събитие ---");
+
+            Console.Write("Въведете име на събитието: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Въведете местоположение: ");
+            string location = Console.ReadLine();
+
+
+            Console.Write("Въведете дата и час (ДД.ММ.ГГГГ ЧЧ:ММ): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            
+            
+               
+                
+            
+
+            Console.Write("Въведете наличен брой билети: ");
+            int tickets = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                
+                if (tickets >= 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Невалиден брой билети. Моля, въведете положително число.");
+                }
+            }
+
+            Console.Write("Въведете цена на билет (в ЛВ): ");
+            decimal price = decimal.Parse(Console.ReadLine());
+            while (true)
+            {
+               
+                if (price >= 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Невалидна цена. Моля, въведете положително число.");
+                }
+            }
+
+
+
+        }
     }
 }
+
+
+       
