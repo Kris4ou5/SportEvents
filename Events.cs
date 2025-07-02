dfb78705c4558e4c8fbe197b970400a72415e5c5
Bozhidar Enev <enevbozhidar@gmail.com>
2025-07-02 18:28:52 +0300

    Added class Events

---
+++ Events.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportEvents
+{
+    internal class Events
+    {
+        public string  eventID {  get; set; }
+        public string Name {  get; set; }
+        public string Location { get; set; }
+
+        public DateTime Date { get; set; }
+         
+        public int TicketsAvailable { get; set; }
+
+        public decimal  Price { get; set; }
+
+        public Events(string name,string location, DateTime date, int ticketsAvailable, decimal price)
+        {
+            eventID = Guid.NewGuid().ToString();
+            Name = name;
+            Location = location;
+            Date = date;
+            TicketsAvailable = ticketsAvailable;
+            Price = price;
+        }
+    }
+}
