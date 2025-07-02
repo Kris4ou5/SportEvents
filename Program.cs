a0dce08dc019a943d5542a8231ca2d3a59f822b3
крис <krisiivanov08@gmail.com>
2025-07-02 19:23:14 +0300

    Added execution of all functions

--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
+            string input;
+            while ((input = Console.ReadLine()) != "x") 
+            { 
+              switch(input) 
+              {
+                    case "1": //добавяне на събитие
+                        break;
+                    case "2": //Продажба на билет
+                        break;
+                    case "3": // проверка на наличноста на билетите
+                        break;
+                    case "4": //Справка
+                        break;
+                    case "5": //kak da zatvorim programata
+                        break;
+                    default: Console.WriteLine("Напиши цифра от 1-5 или X за да продължиш или затвориш");  break;
+                
+              }
             
+            
+            }
         }
     }
 }
