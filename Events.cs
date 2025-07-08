052547a2b1491e2cf999b16a34a063b57673fc15
Bozhidar Enev <enevbozhidar@gmail.com>
2025-07-08 22:51:13 +0300

    Added coments in Events.cs

--- Events.cs
+++ Events.cs
@@ -7,29 +7,39 @@ using System.Threading.Tasks;
 
 namespace SportEvents
 {
+   
     internal class Events
     {
-        public string  eventID {  get; set; }
-        public string Name {  get; set; }
+        // Уникален идентификатор за събитието (генерира се автоматично при създаване)
+        public string eventID { get; set; }
+
+        // Име на събитието
+        public string Name { get; set; }
+
+        // Локация (място) на провеждане на събитието
         public string Location { get; set; }
 
+        // Дата и час на събитието
         public DateTime Date { get; set; }
-         
+
+        // Наличен брой билети за събитието
         public int TicketsAvailable { get; set; }
 
-        public decimal  Price { get; set; }
+        // Цена на един билет за събитието
+        public decimal Price { get; set; }
 
-        public Events(string name,string location, DateTime date, int ticketsAvailable, decimal price)
+       
+        public Events(string name, string location, DateTime date, int ticketsAvailable, decimal price)
         {
+            // Генериране на уникален идентификатор за събитието
             eventID = Guid.NewGuid().ToString();
+
+            // Задаване на стойности за останалите свойства
             Name = name;
             Location = location;
             Date = date;
             TicketsAvailable = ticketsAvailable;
             Price = price;
         }
-
-      
-
     }
 }
